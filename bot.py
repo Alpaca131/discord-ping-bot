b51a87e85a36a8eee2f5651cee7ae3be31ee72a4
Alpaca131 <66709717+Alpaca131@users.noreply.github.com>
2020-09-17 20:35:22 +0900

    英語対応

--- bot.py
+++ bot.py
@@ -20,16 +20,21 @@ async def on_ready():
 async def on_message(message):
     if message.content == 'd.help':
         embed = discord.Embed(title='ヘルプ', color=discord.Color.blue())
-        embed.add_field(name='d.status', value='Discordの全サーバーのステータスを表示します')
-        embed.add_field(name='d.ping', value='Discord(API)の現在のPingを表示します')
-        embed.add_field(name='d.vc', value='このサーバーのVCのPINGを表示します')
+        embed.add_field(name='d.status', value="Discordの全サーバーのステータスを表示します。\nSend discord's server status.")
+        embed.add_field(name='d.ping', value='Discord(API)の現在のPingを表示します。\nSend discord ping.')
+        embed.add_field(name='d.vc', value="このサーバーのVCのPINGを表示します。\nSend this server's region ping.")
+        embed.add_field(name='d.invite', value='このBotの招待リンクを送信します。\nSend invite link of this Bot.')
         await message.channel.send(embed=embed)
+        return
+    elif message.content == 'd.invite':
+        await message.channel.send('こちらが招待リンクです。\nInvite link:\n'
+                                   'https://discord.com/api/oauth2/authorize?client_id=756102269082271746&permissions=281664&scope=bot')
         return
     elif message.content == 'd.ping':
         r = await request(url='https://discordstatus.com/metrics-display/ztt4777v23lf/day.json')
         request_dict = json.loads(r)
         latency = request_dict['metrics'][0]['data'][-1]['value']
-        await message.channel.send('Discordの現在のPingは___**' + str(latency) + 'ms**___です！')
+        await message.channel.send('Ping:___**' + str(latency) + 'ms**___')
         return
     elif message.content == 'd.status':
         r = await request(url='https://srhpyqt94yxb.statuspage.io/api/v2/summary.json')
@@ -43,7 +48,7 @@ async def on_message(message):
         return
     elif message.content == 'd.vc':
         r = await request(url='https://srhpyqt94yxb.statuspage.io/api/v2/summary.json')
-        status = '取得できませんでした'
+        status = "取得できませんでした\nError: couldn't get the data."
         vc_status = ''
         request_dict = json.loads(r)
         for val in request_dict['components']:
@@ -52,8 +57,8 @@ async def on_message(message):
                 status = val['status']
             elif name == 'Voice':
                 vc_status = val['status']
-        embed = discord.Embed(title='このサーバーのリージョン：' + str(message.guild.region), color=discord.Color.blue())
-        embed.add_field(name='ステータス', value=status)
+        embed = discord.Embed(title='Region：' + str(message.guild.region), color=discord.Color.blue())
+        embed.add_field(name='Status', value=status)
         embed.add_field(name='Voice', value=vc_status)
         await message.channel.send(embed=embed)
         return
